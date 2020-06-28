256eca9548058926742d82ffdf9d13bf3f2fa2bb
Sundar Palani <sundar.palani@analog.com>
2020-06-28 03:10:10 -0400

    using cross compiler from apt-get (for CI)

--- aducm3029.cmake
+++ aducm3029.cmake
@@ -18,11 +18,11 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 # automatically set to true if build platform is different from target platform
 # SET(CMAKE_CROSSCOMPILING 1)
 
-set(TOOLS_ROOT /Users/spalani/Library/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/8.2.1-1.7.1/.content)
+# set(TOOLS_ROOT /Users/spalani/Library/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/8.2.1-1.7.1/.content)
 set(TRIPLE arm-none-eabi)
 
-set(CMAKE_C_COMPILER ${TOOLS_ROOT}/bin/${TRIPLE}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLS_ROOT}/bin/${TRIPLE}-g++)
+set(CMAKE_C_COMPILER ${TRIPLE}-gcc)
+set(CMAKE_CXX_COMPILER ${TRIPLE}-g++)
 # set(CMAKE_SYSROOT ${tools}) # optional, and may be specified if a sysroot is available.
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
