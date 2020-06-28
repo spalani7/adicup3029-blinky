f878d310cbd39264234958f3003828daff7df9c7
Sundar Palani <sundar.palani@analog.com>
2020-06-28 01:41:21 -0400

    adding CMSIS pack and updating paths in CMakeLists.txt; working and tested on EVAL-ADICUP3029

--- aducm3029.cmake
+++ aducm3029.cmake
@@ -18,7 +18,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 # automatically set to true if build platform is different from target platform
 # SET(CMAKE_CROSSCOMPILING 1)
 
-set(TOOLS_ROOT /Users/spalani/Downloads/gcc-arm-none-eabi-9-2019-q4-major)
+set(TOOLS_ROOT /Users/spalani/Library/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/8.2.1-1.7.1/.content)
 set(TRIPLE arm-none-eabi)
 
 set(CMAKE_C_COMPILER ${TOOLS_ROOT}/bin/${TRIPLE}-gcc)
@@ -29,15 +29,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
-set(PACK_ROOT "/Users/spalani/embroot/")
-set(CSP "${PACK_ROOT}/ARM.CMSIS.5.4.0")
-set(BSP "${PACK_ROOT}/AnalogDevices.EVAL-ADICUP3029_BSP.1.1.0")
-set(DFP "${PACK_ROOT}/AnalogDevices.ADuCM302x_DFP.3.2.0")
-
-include_directories(${DFP}/Include)
-include_directories(${DFP}/Include/config)
-include_directories(${CSP}/CMSIS/Core/Include/)
 
 set(CMAKE_C_FLAGS "-Wall -ffunction-sections -fdata-sections -ffreestanding -fno-common") #  -DCORE0 -D_RTE_
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcpu=${MCU_ARCH} -mthumb -mfloat-abi=${MCU_FLOAT_ABI} -D__ADUCM3029__ ") #  -DCORE0 -D_RTE_
