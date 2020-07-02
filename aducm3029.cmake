b5f1f8d3ff74ce1007a1b693268f91a6789bf309
Sundar Palani <sundar.palani@analog.com>
2020-07-02 01:43:45 -0400

    updating toolchain paths with env variables to support multiple development environments

--- aducm3029.cmake
+++ aducm3029.cmake
@@ -18,11 +18,16 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 # automatically set to true if build platform is different from target platform
 # SET(CMAKE_CROSSCOMPILING 1)
 
-set(TOOLS_ROOT /Users/spalani/Library/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/8.2.1-1.7.1/.content)
+set(CC_ROOT $ENV{TOOLCHAINROOT})
+if (NOT IS_DIRECTORY ${CC_ROOT})
+    set(CC_ROOT /usr)
+endif()
 set(TRIPLE arm-none-eabi)
 
-set(CMAKE_C_COMPILER ${TOOLS_ROOT}/bin/${TRIPLE}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLS_ROOT}/bin/${TRIPLE}-g++)
+set(TRIPLE arm-none-eabi)
+
+set(CMAKE_C_COMPILER ${CC_ROOT}/bin/${TRIPLE}-gcc)
+set(CMAKE_CXX_COMPILER ${CC_ROOT}/bin/${TRIPLE}-g++)
 # set(CMAKE_SYSROOT ${tools}) # optional, and may be specified if a sysroot is available.
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
